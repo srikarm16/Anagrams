6b3eee8a857ebe1a7c997fcdc0eff2bbf5123c8f
Srikar <hai.srikar@gmail.com>
2021-12-17 16:10:58 -0600

    styling

--- front-end/scripts/index.js
+++ front-end/scripts/index.js
@@ -1,16 +1,35 @@
 window.onload = function () {
   const form = document.getElementById('enter_game_form');
 
-  form.onsubmit = (e) => {
-      const name = e.target.team_name.value;
-      const type = e.target.player_type.value;
-      if (type === "player") {
-          localStorage.setItem('teamName', name);
-          window.location.href = "ready.html";
-      }
-      else {
-        window.location.href = 'spectator.html';
-      }
-      e.preventDefault();
+  document.getElementById('play').onclick = (e) => {
+    const name = document.getElementById('team_name').value;
+    console.log(name);
+    localStorage.setItem('teamName', name);
+    e.preventDefault();
   }
+
+  document.getElementById('spectate').onclick = (e) => {
+    window.location.href = 'spectator.html';
+    e.preventDefault();
+  }
+  // document.getElementById('play').click( () => {
+  //   console.log(`Play Clicked: ${playClicked}`);
+  //   playClicked = true;
+  //   console.log(`Play Clicked: ${playClicked}`);
+  // });
+
+  // form.onsubmit = (e) => {
+  //     const name = e.target.team_name.value;
+  //     const type = e.target.player_type.value;
+  //     console.log(e.target.player_type);
+  //     console.log(`Name: ${name} Type: ${type}`);
+  //     // if (type === "player") {
+  //     //     localStorage.setItem('teamName', name);
+  //     //     window.location.href = "ready.html";
+  //     // }
+  //     // else {
+  //     //   window.location.href = 'spectator.html';
+  //     // }
+  //     e.preventDefault();
+  // }
 }
